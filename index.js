1f4f3fa17a3d3b612ce5e2d347a061d98aa0fd26
ocaocca <ocaocca@gmail.com>
2021-04-08 20:40:07 +0700

    successfully get list of languages and repos accordingly

--- index.js
+++ index.js
@@ -6,9 +6,24 @@ const app = express()
 const port = 3000
 
 app.get('/get/list', async (req, res) => {
-  axios.get('https://api.github.com/search/repositories?q=created:%3E2021-03-01&sort=stars&order=desc?per_page=100')
+  await axios.get('https://api.github.com/search/repositories?q=created:%3E2021-03-01&sort=stars&order=desc?per_page=100')
     .then(result => {
-      res.send(result.data.items[2].language)
+      const getLanguage = []
+      const list = []
+      result.data.items.forEach(item => {
+        getLanguage.push(item.language)
+      })
+      const languages = [...new Set(getLanguage)]
+      languages.forEach(lang => {
+        const repos = []
+        result.data.items.forEach(item => {
+          if (item.language === lang) repos.push(item.html_url)
+        })
+        list.push({
+          languages: lang,
+          repositories: repos
+        })
+      }); res.send(list)
     })
 })
 
